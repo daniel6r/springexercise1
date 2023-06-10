c6afeffb38f8c0868d99aec5e8256b45cb42e4f9
daniel <itsdaniel@protonmail.com>
2023-06-10 18:08:17 +0200

    added user model

--- src/main/java/com/example/springexercise1/model/User.java
+++ src/main/java/com/example/springexercise1/model/User.java
@@ -5,4 +5,33 @@ public class User {
     private String name;
     private boolean registered;
 
+    public User(int id, String name, boolean registered) {
+        this.id = id;
+        this.name = name;
+        this.registered = registered;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(boolean registered) {
+        this.registered = registered;
+    }
 }
